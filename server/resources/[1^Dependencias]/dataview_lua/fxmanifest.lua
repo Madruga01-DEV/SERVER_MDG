d557732bbd53ea4dd6e1ef139dfb98c803cfe87b
Madruga_Dev <madrugadev01@gmail.com>
2025-11-03 00:52:35 -0300

    Update Dependências

---
+++ server/resources/[1^Dependencias]/dataview_lua/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "adamant"
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game "rdr3"
+
+lua54 "yes"
+autor 'mdg_01'
+description '01 não 02'
+version '0.0.1'
+
+client_scripts {
+	'client.lua',
+}
+
